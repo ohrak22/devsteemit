415c7ebbd6fc1f07a776bef0144176e1870b9d0c
John Park <ohrak22@gmail.com>
2018-03-16 11:05:12 +0700

    no message

--- MyBots/PaybackBot/app.js
+++ MyBots/PaybackBot/app.js
@@ -7,38 +7,53 @@ var permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCas
 var arr = [];
 var obj = {
     "postNumber": 1,
-    "lastCommentTime": "",
     "permlinkList": []
 }
 var intervalHours = 3;
 
 console.log('Start');
 
+fs.readFile('./commentData.json', 'utf-8', function (err, data) {
+    if (err)
+        throw err;
+    else if (data) {
+        obj = JSON.parse(data);
+        update();
+        setInterval(update, intervalHours * 60 * 60 * 1000);
+    }
+});
+
 function update() {
     getReward();
-
-    fs.readFile('./commentData.json', 'utf-8', function (error, data) {
-        obj = JSON.parse(data);
-        var nextTime = new Date(obj.lastCommentTime);
-        nextTime.setHours(nextTime.getHours() + intervalHours);
-        var now = new Date();
-
-        if (now > nextTime) {
-            console.log('can comment');
-            broadcastComment();
-        }
-        else {
-            console.log("you need 1 hours");
-        }
-    });
-
+    checkTime();
     payout();
 }
 
-update();
-setInterval(update, intervalHours * 60 * 60 * 1000);
+function checkTime() {
+    steem.api.getDiscussionsByAuthorBeforeDate(author, '', '2017-03-01T00:00:00', 1, function (err, result) {
+        console.log(err, result);
+        if (err) {
+
+        }
+        else if (result) {
+            console.log(result[0].created);
+
+            var nextTime = new Date(result[0].created);
+            nextTime.setHours(nextTime.getHours() + intervalHours);
+            var now = new Date();
+            if (now > nextTime) {
+                console.log('can comment');
+                broadcastComment();
+            }
+            else {
+                console.log("now: " + now + ", next: " + nextTime);
+            }
+        }
+    });
+}
 
 function broadcastComment() {
+    permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
     steem.broadcast.comment(
         config.post,
         '', // Parent Author
@@ -55,10 +70,7 @@ function broadcastComment() {
             }
             else {
                 // comment에 성공하면 리스트에 저장한다. 나중에 보상에 성공하면 지워준다.
-                if (obj.permlinkList.indexOf(permlink) > -1) {
-                    obj.permlinkList.push(permlink);
-                }
-                obj.lastCommentTime = new Date().toISOString();
+                obj.permlinkList.push(permlink);
                 obj.postNumber++;
                 fs.writeFile('./commentData.json', JSON.stringify(obj), function (err) {
                     if (err)
@@ -99,84 +111,51 @@ function getReward() {
     });
 }
 
-
-
 function payout() {
-
-    fs.readFile('./commentData.json', 'utf-8', function (error, data) {
-        obj = JSON.parse(data);
-        for (var i = obj.permlinkList.length - 1; i >= 0; i--) {
-            steem.api.getContent(author, obj.permlinkList[i], function (err, result) {
-                console.log(err, result);
-                if (err) {
-                    throw err;
-                }
-                else if (result) {
-                    var arr = result.total_payout_value.split(' ');
-                    var total_payout_value = parseFloat(arr[0]);
-                    if (total_payout_value > 0.001) { // SBD                        
-                        var reward_balance, recent_claims, basePrice, retSBD;
-                        var msg = "total # of voter : " + result.active_votes.length;    // voters 수를 출력
-                        console.log(msg);
-                        // 보상 풀에 있는 reward 잔액과 보상할 금액 정보 얻기
-                        steem.api.getRewardFund("post", function (err, rewardFund) {
-                            reward_balance = parseFloat(rewardFund.reward_balance.split(' '))
-                            recent_claims = parseInt(rewardFund.recent_claims)
-                            steem.api.getCurrentMedianHistoryPrice(function (err, historyPrice) {
-                                var basePrice = parseFloat(historyPrice.base.split(' '))
-                                var quotePrice = parseFloat(historyPrice.quote.split(' '))
-                                basePrice = basePrice / quotePrice      // feed_price 구함
-                                for (var i = 0; i < result.active_votes.length; i++) {   // voters 수 만큼 반복
-                                    var rshares = parseInt(result.active_votes[i].rshares)   // i번째 voter가 준 shares 값
-                                    // rshares를 SBD로 변환
-                                    retSBD = ((reward_balance / recent_claims) * basePrice * rshares).toFixed(2)
-                                    var str = result.active_votes[i].voter + ' :  $' + retSBD + ' : ' + result.active_votes[i].rshares;
-                                    console.log(str);
-                                    if (retSBD > 0.001) {
-                                        transfer(result.active_votes[i].voter, retSBD, result.title);
-                                    }
+    for (var i = obj.permlinkList.length - 1; i >= 0; i--) {
+        steem.api.getContent(author, obj.permlinkList[i], function (err, result) {
+            console.log(err, result);
+            if (err) {
+                throw err;
+            }
+            else if (result) {
+                var arr = result.total_payout_value.split(' ');
+                var total_payout_value = parseFloat(arr[0]);
+                if (total_payout_value > 0.001) { // SBD                        
+                    var reward_balance, recent_claims, basePrice, retSBD;
+                    var msg = "total # of voter : " + result.active_votes.length;    // voters 수를 출력
+                    console.log(msg);
+                    // 보상 풀에 있는 reward 잔액과 보상할 금액 정보 얻기
+                    steem.api.getRewardFund("post", function (err, rewardFund) {
+                        reward_balance = parseFloat(rewardFund.reward_balance.split(' '))
+                        recent_claims = parseInt(rewardFund.recent_claims)
+                        steem.api.getCurrentMedianHistoryPrice(function (err, historyPrice) {
+                            var basePrice = parseFloat(historyPrice.base.split(' '))
+                            var quotePrice = parseFloat(historyPrice.quote.split(' '))
+                            basePrice = basePrice / quotePrice      // feed_price 구함
+                            for (var i = 0; i < result.active_votes.length; i++) {   // voters 수 만큼 반복
+                                var rshares = parseInt(result.active_votes[i].rshares)   // i번째 voter가 준 shares 값
+                                // rshares를 SBD로 변환
+                                retSBD = ((reward_balance / recent_claims) * basePrice * rshares).toFixed(2)
+                                var str = result.active_votes[i].voter + ' :  $' + retSBD + ' : ' + result.active_votes[i].rshares;
+                                console.log(str);
+                                if (retSBD > 0.001) {
+                                    transfer(result.active_votes[i].voter, retSBD, result.title);
                                 }
-                            });
+                            }
                         });
+                    });
 
-                        obj.permlinkList.splice(i, 1);
-                        fs.writeFile('./commentData.json', JSON.stringify(obj), function (err) {
-                            if (err)
-                                throw err;
-                            console.log('File write completed2');
-                        });
-                    }
+                    obj.permlinkList.splice(i, 1);
+                    fs.writeFile('./commentData.json', JSON.stringify(obj), function (err) {
+                        if (err)
+                            throw err;
+                        console.log('File write completed2');
+                    });
                 }
-            });
-
-            //steem.api.getActiveVotes(author, obj.permlinkList[i], function (err, voters) {
-            //    var reward_balance, recent_claims, basePrice, retSBD;
-            //    var msg = "total # of voter : " + voters.length;    // voters 수를 출력
-            //    console.log(msg);
-            //    // 보상 풀에 있는 reward 잔액과 보상할 금액 정보 얻기
-            //    steem.api.getRewardFund("post", function (err, rewardFund) {
-            //        reward_balance = parseFloat(rewardFund.reward_balance.split(' '))
-            //        recent_claims = parseInt(rewardFund.recent_claims)
-            //        steem.api.getCurrentMedianHistoryPrice(function (err, historyPrice) {
-            //            var basePrice = parseFloat(historyPrice.base.split(' '))
-            //            var quotePrice = parseFloat(historyPrice.quote.split(' '))
-            //            basePrice = basePrice / quotePrice      // feed_price 구함
-            //            for (var i = 0; i < voters.length; i++) {   // voters 수 만큼 반복
-            //                var rshares = parseInt(voters[i].rshares)   // i번째 voter가 준 shares 값
-            //                // rshares를 SBD로 변환
-            //                retSBD = ((reward_balance / recent_claims) * basePrice * rshares).toFixed(2)
-            //                var str = voters[i].voter + ' :  $' + retSBD + ' : ' + voters[i].rshares;
-            //                console.log(str);
-            //                if (retSBD > 0.001) {
-            //                    transfer(voters[i].voter, retSBD);
-            //                }
-            //            }
-            //        });
-            //    });
-            //});
-        }
-
-    });
+            }
+        });
+    }
 }
 
 function transfer(name, value, title) {
